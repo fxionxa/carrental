9d1f4787a7e5cf856aff9a3fa44943f44859c465
Jarvis Lam <102021121+JarvisLam-LemonCEO@users.noreply.github.com>
2023-11-14 02:08:53 -0800

    Update carOwnerACSetting.php

--- Pages/carOwnerACSetting.php
+++ Pages/carOwnerACSetting.php
@@ -6,7 +6,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Account Settings</title>
-<link rel="stylesheet" href="carOwnerACSetting.css">
+<link rel="stylesheet" href="Style/carOwnerACSetting.css">
 </head>
 <body>
     <header>
