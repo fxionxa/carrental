3989fb82b216c6fd14afaff8e6dcf9b4e0e67991
Jarvis Lam <102021121+JarvisLam-LemonCEO@users.noreply.github.com>
2023-11-14 02:10:07 -0800

    Update register.php

--- Pages/register.php
+++ Pages/register.php
@@ -9,7 +9,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Registration Page</title>
-    <link rel="stylesheet" href="style/register.css">
+    <link rel="stylesheet" href="Style/register.css">
     <script>
         // SHA-256 hashing function
         function sha256(plain) {
