b7e99e2340b03e46eaf9cc5fb2cc48c9577d1657
Jarvis Lam <102021121+JarvisLam-LemonCEO@users.noreply.github.com>
2023-11-14 02:10:33 -0800

    Update viewReports.php

--- Pages/viewReports.php
+++ Pages/viewReports.php
@@ -6,7 +6,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>View Reports</title>
-    <link rel="stylesheet" href="viewReports.css">
+    <link rel="stylesheet" href="Style/viewReports.css">
 </head>
 <body>
     <header>
