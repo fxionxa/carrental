bdc42fc3ae13d47d113398ac408c678143678c73
Jarvis Lam <102021121+JarvisLam-LemonCEO@users.noreply.github.com>
2023-11-14 02:09:55 -0800

    Update manageUsers.php

--- Pages/manageUsers.php
+++ Pages/manageUsers.php
@@ -6,7 +6,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Manage Users</title>
-<link rel="stylesheet" href="manageUsers.css">
+<link rel="stylesheet" href="Style/manageUsers.css">
 </head>
 <body>
     <header>
