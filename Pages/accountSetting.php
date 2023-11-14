8144eb9baf1a6f1019e72057accbea35ad18d0f4
Jarvis Lam <102021121+JarvisLam-LemonCEO@users.noreply.github.com>
2023-11-14 02:07:48 -0800

    Update accountSetting.php

--- Pages/accountSetting.php
+++ Pages/accountSetting.php
@@ -6,7 +6,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Account Settings</title>
-    <link rel="stylesheet" href="accountSetting.css">
+    <link rel="stylesheet" href="Style/accountSetting.css">
 </head>
 <body>
     <header>
