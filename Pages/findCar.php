8f9e572fbab2a48344a4607e62cd62a0ba4cb43f
Jarvis Lam <102021121+JarvisLam-LemonCEO@users.noreply.github.com>
2023-11-14 02:09:27 -0800

    Update findCar.php

--- Pages/findCar.php
+++ Pages/findCar.php
@@ -6,7 +6,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Find a Car to Rent</title>
-<link rel="stylesheet" href="findCar.css">
+<link rel="stylesheet" href="Style/findCar.css">
 </head>
 <body>
     <header>
