01674057a137d4a76f4504b5262240ff7c2ffd99
Jarvis Lam <102021121+JarvisLam-LemonCEO@users.noreply.github.com>
2023-11-14 01:54:26 -0800

    Update viewBooking.php without style in it

--- Pages/viewBooking.php
+++ Pages/viewBooking.php
@@ -6,75 +6,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>View Bookings</title>
-    <style>
-        body {
-            font-family: Arial, sans-serif;
-            background-color: #f5f5f5;
-            margin: 0;
-            padding: 0;
-        }
-
-        header {
-            background-color: #007BFF;
-            color: #fff;
-            padding: 10px;
-            text-align: center;
-        }
-
-        h1 {
-            margin: 0;
-        }
-
-        .container {
-            max-width: 800px;
-            margin: 20px auto;
-            padding: 20px;
-            background-color: #fff;
-            border: 1px solid #ccc;
-            border-radius: 5px;
-        }
-
-        h2 {
-            color: #333;
-        }
-
-        p {
-            margin: 0;
-        }
-
-        .button {
-            background-color: #007BFF;
-            color: #fff;
-            border: none;
-            border-radius: 5px;
-            padding: 10px 20px;
-            cursor: pointer;
-            transition: background-color 0.3s; /* Add a transition for background color */
-        }
-
-        .button:hover {
-            background-color: #0056b3; /* Change background color on hover */
-        }
-
-        .back-button {
-            background-color: #007BFF;
-            color: #fff;
-            border: none;
-            border-radius: 5px;
-            padding: 10px 20px;
-            cursor: pointer;
-            transition: background-color 0.3s; /* Add a transition for background color */
-        }
-
-        .back-button:hover {
-            background-color: #0056b3; /* Change background color on hover */
-        }
-
-        a {
-            text-decoration: none;
-            color: #fff;
-        }
-    </style>
+<link rel="stylesheet" href="viewBooking.css">
 </head>
 <body>
     <header>
