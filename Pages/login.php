aba74a960d3557babd5ffc2bf8dc1fbfd538448a
Jarvis Lam <102021121+JarvisLam-LemonCEO@users.noreply.github.com>
2023-11-14 02:09:46 -0800

    Update login.php

--- Pages/login.php
+++ Pages/login.php
@@ -6,7 +6,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login Page</title>
-    <link rel="stylesheet" href="style/login.css">
+    <link rel="stylesheet" href="Style/login.css">
     
 </head>
 <body>
