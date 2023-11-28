55795476be0a6639172971ef61be9726a8d44cd3
mannydwal209 <105015664+mannydwal209@users.noreply.github.com>
2023-11-27 18:47:14 -0800

    added multi-sql statements

--- Routes/process_addListing.php
+++ Routes/process_addListing.php
@@ -1,41 +1,56 @@
 <?php
-    session_start();
+session_start();
 
-    unset($_SESSION["add_error"]);
+unset($_SESSION["add_error"]);
 
-    function test_input($data) {
-        $data = trim($data);
-        $data = stripslashes($data);
-        $data = htmlspecialchars($data);
-        return $data;
+function test_input($data) {
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+    return $data;
+}
+
+if (!isset($_SESSION["user_id"])) {
+    header('Location: '.$uri.'/carrental/pages/login.php');
+    exit;
+}
+
+if ((isset($_POST["carID"])) && (isset($_POST["startDate"])) && (isset($_POST["pricePerDay"]))) {
+
+    require "../../credentials.php";
+
+    $conn = mysqli_connect($host, $user, $pass, $name);
+
+    if (!$conn) {
+        die("Connection failed: " . mysqli_connect_error());
     }
 
-    if (!isset($_SESSION["user_id"])) {
-        header('Location: '.$uri.'/carrental/pages/login.php');
-        exit;
-    }
+    // begin transaction
+    mysqli_begin_transaction($conn);
 
-    if ((isset($_POST["carID"])) && (isset($_POST["startDate"])) && (isset($_POST["pricePerDay"]))) {
-        
-        require "../../credentials.php";
+    // set autocommit
+    mysqli_autocommit($conn, false);
 
-        $conn = mysqli_connect($host, $user, $pass, $name);
+    $carID = test_input($_POST["carID"]);
+    $startDate = test_input($_POST["startDate"]);
+    $pricePerDay = test_input($_POST["pricePerDay"]);
+    $uid = $_SESSION["user_id"];
 
-        if (!$conn) { 
-            die("Connection failed: " . mysqli_connect_error());
-        }
+    $sql = "INSERT INTO listings VALUES(null, $carID, '$startDate', '$pricePerDay', DEFAULT, $uid)";
+    $results = mysqli_query($conn, $sql);
 
-        $carID = test_input($_POST["carID"]);
-        $startDate = test_input($_POST["startDate"]);
-        $pricePerDay = test_input($_POST["pricePerDay"]);
-        $uid = $_SESSION["user_id"];
-
-        $sql = "INSERT INTO listings VALUES(null, $carID, '$startDate', '$pricePerDay', DEFAULT, $uid)";
-        $results = mysqli_query($conn, $sql);
-
+    if ($results) {
+        // commit on success
+        mysqli_commit($conn);
         header('Location: '.$uri.'/carrental/pages/carListing.php');
-        exit();    
+        exit();
+    } else {
+        // rollback on failure
+        mysqli_rollback($conn);
+        header('Location: '.$uri.'/carrental/pages/error.php');
+        exit();
     }
-    header('Location: '.$uri.'/carrental/pages/carListing.php');
-    exit();
+}
+header('Location: '.$uri.'/carrental/pages/carListing.php');
+exit();
 ?>
--- Routes/process_booking.php
+++ Routes/process_booking.php
@@ -1,42 +1,59 @@
 <?php
-    session_start();
+session_start();
+unset($_SESSION["add_error"]);
 
-    unset($_SESSION["add_error"]);
+function test_input($data) {
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+    return $data;
+}
 
-    function test_input($data) {
-        $data = trim($data);
-        $data = stripslashes($data);
-        $data = htmlspecialchars($data);
-        return $data;
+if (!isset($_SESSION["user_id"])) {
+    header('Location: '.$uri.'/carrental/pages/login.php');
+    exit;
+}
+
+if ((isset($_POST["listingID"])) && (isset($_POST["startDate"])) && (isset($_POST["endDate"]))) {
+    require "../../credentials.php";
+
+    $conn = mysqli_connect($host, $user, $pass, $name);
+
+    if (!$conn) {
+        die("Connection failed: " . mysqli_connect_error());
     }
 
-    if (!isset($_SESSION["user_id"])) {
-        header('Location: '.$uri.'/carrental/pages/login.php');
-        exit;
+    mysqli_autocommit($conn, false); // Turn off autocommit to start a transaction
+
+    $listingID = test_input($_POST["listingID"]);
+    $startDate = test_input($_POST["startDate"]);
+    $endDate = test_input($_POST["endDate"]);
+    $uid = $_SESSION["user_id"];
+
+    $success = true;
+
+    // begin a transaction
+    mysqli_begin_transaction($conn);
+
+    $sql1 = "INSERT INTO bookings VALUES(null, $listingID, '$uid', '$startDate', '$endDate')";
+    $result1 = mysqli_query($conn, $sql1);
+
+    if (!$result1) {
+        $success = false;
     }
 
-    if ((isset($_POST["listingID"])) && (isset($_POST["startDate"])) && (isset($_POST["endDate"]))) {
-        
-        require "../../credentials.php";
 
-        $conn = mysqli_connect($host, $user, $pass, $name);
-
-        if (!$conn) { 
-            die("Connection failed: " . mysqli_connect_error());
-        }
-
-        $listingID = test_input($_POST["listingID"]);
-        $startDate = test_input($_POST["startDate"]);
-        $endDate = test_input($_POST["endDate"]);
-        $uid = $_SESSION["user_id"];
-
-
-        $sql = "INSERT INTO bookings VALUES(null, $listingID, '$uid', '$startDate', '$endDate')";
-        $results = mysqli_query($conn, $sql);
-
+    if ($success) {
+        mysqli_commit($conn); // commit on success
         header('Location: '.$uri.'/carrental/pages/findCar.php');
-        exit();    
+        exit();
+    } else {
+        mysqli_rollback($conn); // rollback on failure
+        header('Location: '.$uri.'/carrental/pages/error.php');
+        exit();
     }
-    header('Location: '.$uri.'/carrental/pages/findCar.php');
-    exit();
+}
+header('Location: '.$uri.'/carrental/pages/findCar.php');
+exit();
+
 ?>
